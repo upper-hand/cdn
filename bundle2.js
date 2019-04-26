c5f692a381ad1f8831e941aecfcd1ec4a9a12143
lyumn <lyumn@users.noreply.github.com>
2019-04-26 16:46:22 -0400

    Update bundle2.js

--- bundle2.js
+++ bundle2.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2dc2c346376baf147b03";
+/******/ 	var hotCurrentHash = "fc11369bacff74d005f3";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
